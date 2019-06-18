aeb113022edcef09a1e7f353cbd851e5f95d4243
Lokesh Thummala <41089782+ThummalaLokesh@users.noreply.github.com>
2019-06-17 20:12:52 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+/* 
+    File name = index.js
+    Author's name = Lokesh Thummala
+    web site name = lokesh-thummala-portfolio
+    file description = This is main index javascript file to my webpage. It has routing to  the website 
+*/
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Lokesh Thummala' });
 });
 
 module.exports = router;
